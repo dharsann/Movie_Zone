ed9327abb833059f894f1321f66b3d8868dbe5fe
dharsann <131436118+dharsann@users.noreply.github.com>
2024-04-17 20:32:32 +0530

    Update Login.js

--- client/src/Pages/Login.js
+++ client/src/Pages/Login.js
@@ -1,40 +1,41 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 
 const Login = ({ onLogin }) => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false); // Add isLoading state
   const navigate = useNavigate();
 
-  // Storing the token in localStorage after successful login
-const handleLogin = async () => {
-  try {
-    const response = await fetch('http://localhost:5000/api/signin', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ username, password })
-    });
+  const handleLogin = async () => {
+    setIsLoading(true); // Set loading state to true when login process starts
+    try {
+      const response = await fetch("http://localhost:5000/api/signin", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
+      });
 
-    const data = await response.json();
+      const data = await response.json();
 
-    if (response.ok) {
-      // Set token in localStorage
-      localStorage.setItem("token", data.token);
-
-      onLogin(); // Call the onLogin callback function passed from App
-      navigate('/');
-    } else {
-      setError(data.msg || 'Invalid username or password');
+      if (response.ok) {
+        localStorage.setItem("token", data.token);
+        localStorage.setItem("isLoggedIn", "true");
+        onLogin();
+        navigate("/trending");
+      } else {
+        setError(data.msg || "Invalid username or password");
+      }
+    } catch (error) {
+      console.error("Login error:", error);
+      setError("An error occurred during login");
+    } finally {
+      setIsLoading(false); // Set loading state back to false when login process finishes
     }
-  } catch (error) {
-    console.error('Login error:', error);
-    setError('An error occurred during login');
-  }
-};
-
+  };
 
   return (
     <div className="container d-flex justify-content-center align-items-center vh-100">
@@ -57,8 +58,9 @@ const handleLogin = async () => {
             className="form-control"
           />
           <br />
-          <button onClick={handleLogin} className="btn btn-primary">
-            Login
+          {/* Disable the button when loading */}
+          <button onClick={handleLogin} className="btn btn-primary" disabled={isLoading}>
+            {isLoading ? "Logging in..." : "Login"}
           </button>
           {error && <div className="text-danger">{error}</div>}
           <br />
